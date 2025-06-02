7656fc07eb47d7fc7fc1f82677e4d935d0cface7
qinghuiKAKA <49874177+qinghuiKAKA@users.noreply.github.com>
2025-06-02 18:50:55 +0800

    Update 2048助手.user.js
    增加屏蔽词

--- 2048助手.user.js
+++ 2048助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         2048助手
 // @description  2048论坛直接预览帖子内图片，过滤低质量帖子
-// @version      1.0.0
+// @version      1.0.1
 // @author       qinghuiKAKA
 // @namespace    hjd2048.com
 // @match        *://hjd2048.com/*
@@ -43,19 +43,21 @@
     //过滤搜索或者板块内的垃圾内容，垃圾板块，空板块
     if (href.indexOf("search.php") >= 0) {
       var thattd = that.find("th:eq(0)");
-      if (that.find("td:eq(1)").text().match(/豆芽高手/i) || that.find("td:eq(0)").find('a[href*="fid=0"]').length > 0 || that.find("td:eq(0)").text().match(/中字原創|H-GAME|日本騎兵|三級寫真|卡通漫畫|实用漫画|多挂原创|歐美新片|歐美激情|熟女专图|高跟絲襪|街拍精品|网盘二区|网盘三区|高清有碼|AI視界|A I 智能|真实街拍/i)) {
+      if (thattd.text().match(/伪娘|破壊版|有码/i) ||
+          that.find("td:eq(1)").text().match(/豆芽高手/i) ||
+          that.find("td:eq(0)").find('a[href*="fid=0"]').length > 0 ||
+          that.find("td:eq(0)").text().match(/中字原創|H-GAME|日本騎兵|三級寫真|卡通漫畫|实用漫画|多挂原创|歐美新片|歐美激情|熟女专图|高跟絲襪|街拍精品|网盘二区|网盘三区|高清有碼|AI視界|A I 智能|真实街拍|图你所图/i)) {
         that.remove();
         return true;
       }
     }
     else{
       var thattd = that.find("td:eq(1)");
-      /*
-      if (that.find("td:eq(2)").text().match(/国产专员|優衣庫|hdgc|日落黄昏|罗马教皇|avp2p|东华帝君|東方秋白|feixue124|YouKu|soav.|myheroman|zgome|魔火帝皇|豆芽高手|1080fuli/i)) {
+      //that.find("td:eq(2)").text().match(/国产专员|優衣庫|hdgc|日落黄昏|罗马教皇|avp2p|东华帝君|東方秋白|feixue124|YouKu|soav.|myheroman|zgome|魔火帝皇|豆芽高手|1080fuli/i
+      if (thattd.text().match(/伪娘|破壊版|有码/i)) {
         that.remove();
         return true;
       }
-      */
     }
     //第二个td元素为标题,搜索中第一个th为标题
 
