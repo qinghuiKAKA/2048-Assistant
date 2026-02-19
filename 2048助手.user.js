d829cbbb6944eae054b8283008456870d4b56e65
qinghuiKAKA <49874177+qinghuiKAKA@users.noreply.github.com>
2026-02-19 12:56:46 +0800

    新增对附件金币的预览

--- 2048助手.user.js
+++ 2048助手.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         2048助手
 // @description  2048论坛直接预览帖子内图片和金币，过滤低质量帖子
-// @version      1.0.2
+// @version      1.0.3
 // @author       qinghuiKAKA
 // @namespace    hjd2048.com
 // @match        *://hjd2048.com/*
-// @match        *://v5oob.346142.org/*
+// @match        *://oagscj.bjq0uaw.com/*
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
 // @grant        GM_xmlhttpRequest
 // @license 	 GNU GPLv3
@@ -33,8 +33,11 @@
           cookie: document.cookie,
           referer: href,
         };
-
-
+  // 默认不显示官方预览图
+  const checkbox = document.querySelector('input[name="hide_thumb"][value="1"]');
+      if (checkbox) {
+          checkbox.checked = true;
+      }
   // 预览内容图片
   $(".tr3.t_one, .tr3.tac").each(function () {
     var that = $(this);
@@ -43,10 +46,10 @@
     //过滤搜索或者板块内的垃圾内容，垃圾板块，空板块
     if (href.indexOf("search.php") >= 0) {
       var thattd = that.find("th:eq(0)");
-      if (thattd.text().match(/伪娘|破壊版|有码/i) ||
+      if (thattd.text().match(/男娘|人妖|伪娘|破壊版|有码|欧美/i) ||
           that.find("td:eq(1)").text().match(/豆芽高手/i) ||
           that.find("td:eq(0)").find('a[href*="fid=0"]').length > 0 ||
-          that.find("td:eq(0)").text().match(/中字原創|H-GAME|日本騎兵|三級寫真|卡通漫畫|实用漫画|多挂原创|歐美新片|歐美激情|熟女专图|高跟絲襪|街拍精品|网盘二区|网盘三区|高清有碼|AI視界|A I 智能|真实街拍|图你所图/i)) {
+          that.find("td:eq(0)").text().match(/中字原創|H-GAME|日本騎兵|三級寫真|卡通漫畫|实用漫画|正片大片|优质图片|唯美清純|亞洲正妹|美足踩踏|绳艺捆绑|多挂原创|歐美新片|歐美激情|熟女专图|高跟絲襪|街拍精品|网盘二区|网盘三区|高清有碼|AI視界|A I 智能|真实街拍|图你所图|小说/i)) {
         that.remove();
         return true;
       }
@@ -54,7 +57,7 @@
     else{
       var thattd = that.find("td:eq(1)");
       //that.find("td:eq(2)").text().match(/国产专员|優衣庫|hdgc|日落黄昏|罗马教皇|avp2p|东华帝君|東方秋白|feixue124|YouKu|soav.|myheroman|zgome|魔火帝皇|豆芽高手|1080fuli/i
-      if (thattd.text().match(/伪娘|破壊版|有码/i)) {
+      if (thattd.text().match(/男娘|人妖|伪娘|破壊版|有码|欧美/i)) {
         that.remove();
         return true;
       }
@@ -70,15 +73,24 @@
           var doc = result.responseText;
 
           var goldSet = new Set(); // 存储去重的金币字符串
-          // 查找金币字符串（优先在.f14.cc内查找）
-          $(doc).find(".f14.cc *").addBack().contents().filter(function() {
-            return this.nodeType === 3; // 只处理文本节点
-          }).each(function() {
-            const text = this.nodeValue;
-            const matches = text.match(/\d+金币/g);
+          //优先在附件元信息区域查找金币字符串（允许空格：100 金币）
+          $(doc).find(".attach-meta").each(function() {
+            const text = $(this).text();
+            const matches = text.match(/\d+\s*金币/g);
             if (matches) matches.forEach(m => goldSet.add(m));
           });
-          // 如果.f14.cc中没找到，全局查找
+          //如果没找到，继续优先在.f14.cc内查找
+          if (goldSet.size === 0) {
+            $(doc).find(".f14.cc *").addBack().contents().filter(function() {
+              return this.nodeType === 3; // 只处理文本节点
+            }).each(function() {
+              const text = this.nodeValue;
+              //允许数字和"金币"之间有空格,能匹配文本金币格式和附件金币格式
+              const matches = text.match(/\d+金币/g);
+              if (matches) matches.forEach(m => goldSet.add(m));
+            });
+          }
+          // 如果.f14.cc中还是没找到，全局查找
           if (goldSet.size === 0) {
             $(doc).find("*").contents().filter(function() {
               return this.nodeType === 3;
@@ -102,28 +114,18 @@
                 var src = $(this).attr('data-original') !== undefined
                     ? $(this).attr('data-original')
                     : $(this).attr('src');
-                thattd.append(
-                  "<img object-fit='contain' style='width:200px;' src='" +
-                    src +
-                    "' />"
-                );
+                    thattd.append("<img object-fit='contain' style='width:200px;' src='" + src + "' />");
               });
           //多种格式图片
           $(doc).find(".showhide img:not([align='absmiddle'])").each(function (index) {
-                  if (index == 0) {
-                    thattd.append("<br />");
-                  }
-                  var src = $(this).attr('data-original') !== undefined
-                      ? $(this).attr('data-original')
-                      : $(this).attr('src');
-                  thattd.append(
-                    "<img object-fit='contain' style='width:200px;' src='" +
-                      src +
-                      "' />"
-                  );
-                });
-
-
+                if (index == 0) {
+                  thattd.append("<br />");
+                }
+                var src = $(this).attr('data-original') !== undefined
+                    ? $(this).attr('data-original')
+                    : $(this).attr('src');
+                    thattd.append("<img object-fit='contain' style='width:200px;' src='" + src + "' />");
+              });
         }
       });
     }
